c099178c207545f735d361d10dfd63bb8d77ed3e
harish <harishgoud1411@gmail.com>
2024-04-04 13:43:41 +0000

    unable to change  date format

---
+++ app.js
@@ -0,0 +1,128 @@
+const express = require('express')
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+const path = require('path')
+const {format, compareAsc} = require('date-fns')
+const app = express()
+app.use(express.json())
+let db
+const dbPath = path.join(__dirname, 'todoApplication.db')
+const initializeServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    })
+    app.listen(3000, () => {
+      console.log('Server Started!!!')
+    })
+  } catch (e) {
+    console.log(`Error DB : ${e.message}`)
+    process.exit(-1)
+  }
+}
+initializeServer()
+
+app.get('/todos/', async (request, response) => {
+  const {
+    status = '',
+    priority = '',
+    category = '',
+    search_q = '',
+    dueDate = '',
+  } = request.query
+  const query1 = `
+  SELECT * FROM todo 
+  WHERE todo LIKE '%${search_q}%' AND
+  status LIKE '%${status}%'AND
+  priority LIKE '%${priority}%'AND
+  category LIKE '%${category}%';`
+  const result1 = await db.all(query1)
+  response.send(result1)
+})
+
+app.get('/todos/:todoId/', async (request, response) => {
+  const {todoId} = request.params
+  const query2 = `
+  SELECT * FROM todo 
+  WHERE id = ${todoId};
+  `
+  const result2 = await db.all(query2)
+  response.send(result2)
+})
+
+app.get('/agenda/', async (request, response) => {
+  const {date} = request.query
+  const query3 = `
+  SELECT * FROM todo 
+  WHERE due_date  LIKE '${date}';`
+  const result3 = await db.all(query3)
+  response.send(result3)
+})
+
+app.post('/todos/', async (request, response) => {
+  const {id, todo, priority, status, category, dueDate} = request.body
+  const query4 = `
+  INSERT INTO todo(id,todo,category,priority,status,due_date)
+  VALUES (${id},'${todo}','${priority}','${status}','${category}','${dueDate}');`
+  await db.run(query4)
+  response.send('Todo Successfully Added')
+})
+
+app.put('/todos/:todoId/', async (request, response) => {
+  const {status, priority, todo, category, dueDate} = request.body
+  const {todoId} = request.params
+  let query5
+  if (status !== undefined) {
+    query5 = `
+    UPDATE todo
+    SET 
+    status = '${status}'
+    WHERE id = ${todoId};`
+    await db.run(query5)
+    response.send('Status Updated')
+  } else if (priority !== undefined) {
+    query5 = `
+    UPDATE todo
+    SET 
+    priority = '${priority}'
+    WHERE id = ${todoId};`
+    await db.run(query5)
+    response.send('Priority Updated')
+  } else if (todo !== undefined) {
+    query5 = `
+    UPDATE todo
+    SET 
+    todo = '${todo}'
+    WHERE id = ${todoId};`
+    await db.run(query5)
+    response.send('Todo Updated')
+  } else if (category !== undefined) {
+    query5 = `
+    UPDATE todo
+    SET 
+    category = '${category}'
+    WHERE id = ${todoId};`
+    await db.run(query5)
+    response.send('Category Updated')
+  } else if (dueDate !== undefined) {
+    query5 = `
+    UPDATE todo
+    SET 
+    due_date = '${dueDate}'
+    WHERE id = ${todoId};`
+    await db.run(query5)
+    response.send('Due Date Updated')
+  }
+})
+
+app.delete('/todos/:todoId/', async (request, response) => {
+  const {todoId} = request.params
+  const query6 = `
+  DELETE FROM todo
+  WHERE id = ${todoId};`
+  await db.run(query6)
+  response.send('Todo Deleted')
+})
+
+module.exports = app
